f51aa829b933ed2d8e6db7ff4d57b198ba7142b4
Gaspard Cabecas-Segura <gcabecas@z4r8p4.42lyon.fr>
2025-12-10 12:37:16 +0100

    push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gcabecas <gcabecas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:43:47 by gcabecas          #+#    #+#             */
-/*   Updated: 2025/12/10 12:12:02 by gcabecas         ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 12:19:12 by gcabecas         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ int	main(int argc, char **argv)
 	stack_b.tail = NULL;
 	printf("%.2f\n", compute_disorder(&stack_a));
 	tmp = stack_a.head;
-	while (tmp)
-	{
-		printf("%i\n", tmp->nbr);
-		tmp = tmp->nxt;
-	}
+	// while (tmp)
+	// {
+	// 	printf("%i\n", tmp->nbr);
+	// 	tmp = tmp->nxt;
+	// }
 	printf("\n");
 	simple_solver(&stack_a, &stack_b);
 	printf("\n");
 	tmp = stack_a.head;
-	while (tmp)
-	{
-		printf("%i\n", tmp->nbr);
-		tmp = tmp->nxt;
-	}
+	// while (tmp)
+	// {
+	// 	printf("%i\n", tmp->nbr);
+	// 	tmp = tmp->nxt;
+	// }
 	return (0);
 }
--- simple.c
+++ simple.c
@@ -6,7 +6,7 @@
 /*   By: gcabecas <gcabecas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 10:50:25 by gcabecas          #+#    #+#             */
-/*   Updated: 2025/12/10 12:11:28 by gcabecas         ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 12:36:29 by gcabecas         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,13 @@ int	verif(t_stack *stack_a, t_stack *stack_b)
 	return (0);
 }
 
-void	simple_solver(t_stack *stack_a, t_stack *stack_b)
+void	simple_solver(t_stack *stack_a, t_stack *stack_b) //trop d'op
 {
 	int	sorted;
+	int	op;
 
 	sorted = 1;
+	op = 0;
 	while (sorted)
 	{
 		if (stack_a->head == NULL)
@@ -53,5 +55,9 @@ void	simple_solver(t_stack *stack_a, t_stack *stack_b)
 			pb(stack_a, stack_b);
 		else if (!stack_a->head->nxt)
 			pb(stack_a, stack_b);
+		op++;
 	}
+	printf("op = %i\n", op);
 }
+
+//./a.out 100 99 98 97 96 95 94 93 92 91 90 89 88 87 86 85 84 83 82 81 80 79 78 77 76 75 74 73 72 71 70 69 68 67 66 65 64 63 62 61 60 59 58 57 56 55 54 53 52 51 50 49 48 47 46 45 44 43 42 41 40 39 38 37 36 35 34 33 32 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
